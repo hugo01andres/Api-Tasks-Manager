9f82401696f81ddb2abaecd41285544147ddbc38
hugo01andres <hugoandres01@live.com.mx>
2023-11-08 09:02:16 -0600

    Borro los prints para mayor seguridad

--- src/app/config.py
+++ src/app/config.py
@@ -5,7 +5,5 @@ class Config:
     load_dotenv()
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    print(SQLALCHEMY_DATABASE_URI)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
-    print(SECRET_KEY)
